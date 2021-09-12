a3eac55c52014672289520acf93966be002ee709
Adarsh H R <40816614+Adars-H-R@users.noreply.github.com>
2021-09-12 10:02:00 +0530

    Initial commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 30, Location = ResponseCacheLocation.None, NoStore = true)]
+        public async Task<IActionResult> Index(int pageSize = 30)
+        {
+            CarCrashViewModel carCrashView = new CarCrashViewModel();
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    carCrashView.PreviousPageIndex = pageSize > 30 ? pageSize -30: 0;
+                    carCrashView.CurrentPageIndex = pageSize;
+                    // client.BaseAddress = new Uri("https://data.cityofnewyork.us/resource/h9gi-nx95.json");
+                    var apiResponse = await client.GetAsync("https://data.cityofnewyork.us/resource/h9gi-nx95.json");
+                    var data = await apiResponse.Content.ReadAsStringAsync();
+                    //var carCrashView1 = JsonConvert.DeserializeObject<Root>(data);
+                    var desObj = JsonConvert.DeserializeObject<List<CarCrash>>(data);
+                    if(carCrashView.CurrentPageIndex <desObj.Count )
+                    {
+                        carCrashView.CarCrashes = desObj.GetRange(carCrashView.PreviousPageIndex, carCrashView.CurrentPageIndex);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+           
+            return View(carCrashView);
+        }
+
+       
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/Models/CarCrash.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class CarCrashViewModel
+    {
+        public IEnumerable<CarCrash> CarCrashes { get; set; }
+        public int PreviousPageIndex { get; set; }
+        public int CurrentPageIndex { get; set; }
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    
+    public class Location
+    {
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("human_address")]
+        public string HumanAddress { get; set; }
+    }
+
+    public class CarCrash
+    {
+        [JsonProperty("crash_date")]
+        public DateTime CrashDate { get; set; }
+
+        [JsonProperty("crash_time")]
+        public string CrashTime { get; set; }
+
+        [JsonProperty("on_street_name")]
+        public string OnStreetName { get; set; }
+
+        [JsonProperty("number_of_persons_injured")]
+        public string NumberOfPersonsInjured { get; set; }
+
+        [JsonProperty("number_of_persons_killed")]
+        public string NumberOfPersonsKilled { get; set; }
+
+        [JsonProperty("number_of_pedestrians_injured")]
+        public string NumberOfPedestriansInjured { get; set; }
+
+        [JsonProperty("number_of_pedestrians_killed")]
+        public string NumberOfPedestriansKilled { get; set; }
+
+        [JsonProperty("number_of_cyclist_injured")]
+        public string NumberOfCyclistInjured { get; set; }
+
+        [JsonProperty("number_of_cyclist_killed")]
+        public string NumberOfCyclistKilled { get; set; }
+
+        [JsonProperty("number_of_motorist_injured")]
+        public string NumberOfMotoristInjured { get; set; }
+
+        [JsonProperty("number_of_motorist_killed")]
+        public string NumberOfMotoristKilled { get; set; }
+
+        [JsonProperty("contributing_factor_vehicle_1")]
+        public string ContributingFactorVehicle1 { get; set; }
+
+        [JsonProperty("contributing_factor_vehicle_2")]
+        public string ContributingFactorVehicle2 { get; set; }
+
+        [JsonProperty("collision_id")]
+        public string CollisionId { get; set; }
+
+        [JsonProperty("vehicle_type_code1")]
+        public string VehicleTypeCode1 { get; set; }
+
+        [JsonProperty("vehicle_type_code2")]
+        public string VehicleTypeCode2 { get; set; }
+
+        [JsonProperty("borough")]
+        public string Borough { get; set; }
+
+        [JsonProperty("zip_code")]
+        public string ZipCode { get; set; }
+
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+
+        [JsonProperty("cross_street_name")]
+        public string CrossStreetName { get; set; }
+
+        [JsonProperty("off_street_name")]
+        public string OffStreetName { get; set; }
+
+        [JsonProperty("contributing_factor_vehicle_3")]
+        public string ContributingFactorVehicle3 { get; set; }
+
+        [JsonProperty("contributing_factor_vehicle_4")]
+        public string ContributingFactorVehicle4 { get; set; }
+
+        [JsonProperty("vehicle_type_code_3")]
+        public string VehicleTypeCode3 { get; set; }
+
+        [JsonProperty("vehicle_type_code_4")]
+        public string VehicleTypeCode4 { get; set; }
+
+        [JsonProperty("contributing_factor_vehicle_5")]
+        public string ContributingFactorVehicle5 { get; set; }
+
+        [JsonProperty("vehicle_type_code_5")]
+        public string VehicleTypeCode5 { get; set; }
+    }
+}
